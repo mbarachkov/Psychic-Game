98313be7d63a1ddfbd465a3d9887faca989ccb61
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-21 21:08:05 -0700

    progress on page layout and is now logging keys

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,22 @@
-var playerLives = 9;
+// var playerLives = 9; 
 
 var abcArray = "";
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	
-while (abcArray.length < 6) {
+while (abcArray.length < 1) {
 	abcArray += alphabet[Math.floor(Math.random() * alphabet.length)];
 }
-console.log(abcArray);	
+console.log(abcArray);
+
+
+//track what letters are being input
+document.onkeyup = function(event) {
+	console.log(event.key);
+	gSoFar.textContent += event.key
+}
+
+
+
 
 
 //input a letter and keep track of it on "letters guessed so far" if it is ≠ computer letter
