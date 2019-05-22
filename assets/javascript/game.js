fd1937ace565140055267f37a66cb1ee92a8f00d
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-21 20:34:11 -0700

    added a random picker for my alphabet array and console.logged it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,23 @@
-alert("hello")
+var playerLives = 9;
+
+var abcArray = "";
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	
+while (abcArray.length < 6) {
+	abcArray += alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+console.log(abcArray);	
+
+
+//input a letter and keep track of it on "letters guessed so far" if it is ≠ computer letter
+
+//if players letter is === to computer letter
+	//wins + 1
+
+//if players letter ≠ computer letter 
+	//guesses left -1
+
+//if guesses left === 0 
+	//losses + 1
+
+//when +1 loss happens start process over
