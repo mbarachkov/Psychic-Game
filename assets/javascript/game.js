15321c4e0f0454047c3738d06ae2410cca456ead
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-22 19:26:18 -0700

    defining variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins: 0;
+var losses: 0;
+var gLeft: 9;
