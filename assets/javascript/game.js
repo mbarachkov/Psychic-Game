9cc357d6998b1917042dacdd29f411a986b51374
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-22 20:14:54 -0700

    added the letter selector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,13 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var wins: 0;
-var losses: 0;
-var gLeft: 9;
+var lGuessed = [];
+var cGuess = [];
+var wins = 0;
+var losses = 0;
+var gLeft = 9;
+
+window.onload = function() {
+	var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+	cGuess.push(compGuess);
+	console.log(cGuess[0]);
+}
+
