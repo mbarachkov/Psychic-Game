117f78880338b1f899732f15051bb81aba75a4cf
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-25 21:51:32 -0700

    working on the functionality of the reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,20 @@ var cGuess = [];
 var wins = 0;
 var losses = 0;
 var gLeft = 9;
-//the computer uses the randomizer and the .push function to isolate a target letter to be chosen
 window.onload = function () {
 	var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 	cGuess.push(compGuess);
 	console.log(cGuess[0]);
 }
-//the input is tracked by the onkeyup function and the .push isolates the letter that was input
+document.getElementById("rButton").onclick = resetButton;
+
+function resetButton() {
+  document.getElementById("rButton") = ();
+}
 document.onkeyup = function(event) {
 	var pGuess = event.key;
 	lGuessed.push(pGuess);
 	console.log(cGuess[0]);
-	//these are the conditions to a win and to reset all the values back to normal for the next round.
 	if ((pGuess === cGuess[0]) && (gLeft > 0)) {
 		wins++;
 		lGuessed.length = 0;
@@ -26,11 +28,9 @@ document.onkeyup = function(event) {
 		cGuess.push(compGuess);
 		console.log(cGuess[0]);
 	}
-	//this argument will keep track of how many guesses the player has left
 	else if ((pGuess !== cGuess[0]) && (gLeft > 0)) {
 		gLeft = gLeft - 1;
 	}
-	//if all the other conditions are not met you will gain a loss and reset the values back for a new round
 	else {
 		losses++;
 		gLeft = 9;
